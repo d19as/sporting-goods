542466025847c1ebce4af29a5837ee81429b2439
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2023-12-20 23:43:34 +0600

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavLink, Route, Router, Routes } from 'react-router-dom';
+import Register from './Register';
+import Main from './Main';
+import Catalog from './Catalog';
+import Service from './Service';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <a href="#" class="logo">  PlayOff <br />store</a>
+          <div class="bx bx-menu" id="menu-icon"></div>
+          <ul class="navbar">
+            <NavLink to={"/registration"}>
+            <li><a href="#">Registration</a></li>
+            </NavLink>
+            <NavLink to={"/"}>
+            <li><a href="#">Main</a></li>
+            </NavLink>
+            <NavLink to={"/catalog"}>
+            <li><a href="/catalog">Catalog</a></li>
+            </NavLink>
+            <NavLink to={"/service"}>
+            <li><a href="#service">Service</a></li>
+            </NavLink>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </header>
+        <Routes>
+          <Route path="/registration" element={<Register/>} />
+          <Route path="/" element={<Main/>} />
+          <Route path="/catalog" element={<Catalog/>} />
+          <Route path="/service" element={<Service/>} />
+        </Routes>
+        <div class="copyright">
+          <p>PlayOff's All Rights Reserved</p>
+        </div>
+      </div >
   );
 }
 
